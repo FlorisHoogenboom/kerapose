006e8d3d90de4318c9417f94b25707fa4305a493
Floris Hoogenboom <floris@digitaldreamworks.nl>
2019-08-03 15:05:41 +0200

    Setup project scafolding.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+test_requires = [
+    'pytest>=5.0.1'
+]
+
+dev_requires = [
+    'flake8==3.7.8'
+] + test_requires
+
+setup(
+    name='openpose-cycling',
+    version='0.1.0',
+    packages=['keras_han'],
+    url='https://github.com/maxboiten/openpose-cycling',
+    license='MIT',
+    author='Floris Hoogenboom & Max Boiten',
+    author_email='floris@digitaldreamworks.nl',
+    description='An application of pose estimation to cycling images.',
+    install_requires=[
+        'keras>=2.2.4'
+    ],
+    test_requires=test_requires,
+    extras_require={
+        'test': test_requires,
+        'dev': dev_requires
+    }
+)
